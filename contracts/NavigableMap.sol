bc566ffe23c3aca3476bf65f14028dd8625953f9
cymon <cymon@tiscali.it>
2020-03-21 02:26:47 +0100

    Commands form

--- contracts/NavigableMap.sol
+++ contracts/NavigableMap.sol
@@ -14,31 +14,48 @@ contract NavigableMap {
     }
 
     mapping(uint   => Mecha) public armies;
+    mapping(uint => Position) public mecha_positions;
+    mapping(uint => string) public commands;
     mapping(string => Position) public wps;
-    mapping(string => Position) public mecha_positions;
+    mapping(uint => string) public wps_names;
     mapping(string => string) public spawn_points;
     
-    uint armyCounter = 0;
+    uint public armyCounter = 0;
+    uint public waypointCounter = 0;
     
 
     constructor() public {
-        buildWayPoints();
+        _buildWayPoints();
+        _buildArmies();
     }
 
-    function buildWayPoints() private {
+    function _buildWayPoints() private {
         wps["Center"] = Position(0, 0, 0);
+        wps_names[0] = "Center";
         wps["Blue"] = Position(-100000, 0, 0);
+        wps_names[1] = "Blue";
         wps["Red"] = Position(100000, 0, 0);
+        wps_names[2] = "Red";
+        waypointCounter = 3;
         spawn_points["Red"] = "Red";
         spawn_points["Blue"] = "Blue";
+    }
+    
+    function _buildArmies() private {
         _createMecha("Diver", "Blue");
         _createMecha("Zaku", "Red");
     }
+    
 
     function _createMecha(string memory _name, string memory _faction) private {
         armies[armyCounter] = Mecha(_name, _faction);
+        mecha_positions[armyCounter] = wps[spawn_points[_faction]];
         armyCounter++;
-        mecha_positions[_name] = wps[spawn_points[_faction]];
     }
+
+    function addCommand(uint _id, string calldata _command) external {
+        commands[_id] = _command;
+    }
+
 }
 
--- src/js/app.js
+++ src/js/app.js
@@ -42,20 +42,63 @@ App = {
     }).then(function() { $('#mechas').empty(); })
       .then(function() { return gunplaInstance.armies(0)}).then(function(mecha) {
             $('#mechas').append(App.mechaTemplate(0, mecha[0], mecha[1])); 
-            return gunplaInstance.mecha_positions(mecha[0]); })
+            return gunplaInstance.mecha_positions(0); })
       .then(function(pos) {
-            $('#mecha0').append(App.positionTemplate(pos)) })
+            $('#mecha0').append(App.positionTemplate(pos));
+            $('#mecha0').append(App.commandsForm(0)); })
       .then(function() { return gunplaInstance.armies(1)}).then(function(mecha) {
             $('#mechas').append(App.mechaTemplate(1, mecha[0], mecha[1])); 
-            return gunplaInstance.mecha_positions(mecha[0]); })
+            return gunplaInstance.mecha_positions(1); })
       .then(function(pos) {
-            $('#mecha1').append(App.positionTemplate(pos)) })
+            $('#mecha1').append(App.positionTemplate(pos));
+            $('#mecha1').append(App.commandsForm(1)); })
   },
   mechaTemplate: function(id, name, faction) {
-        return  '<p id="mecha' + id + '">'+ name + ' (' + faction + ')</p>';
+    return  '<p id="mecha' + id + '">'+ name + ' (' + faction + ')</p>';
   },
   positionTemplate: function(pos) {
-        return  '<br />[' + pos[0] +', '+pos[1] +', '+pos[2] + ']';
+    return  '<br />[' + pos[0] +', '+pos[1] +', '+pos[2] + ']';
+  },
+  commandsForm : function(id) {
+    return `
+        <form>
+            <input type="hidden" id="id" name="id" value="${id}">
+            <div class="form-group">
+                <label for="commands">Select Command</label>
+                <select class="form-control" name="commands" onchange="App.commandParams(this, ${id})">
+                    <option value="">Select...</option>
+                    <option value="flywp">FLY TO WAYPOINT</option>
+                </select>
+            </div>
+            <div class="form-group">
+            </div>
+            <button type="submit" class="btn btn-primary">Submit</button>
+            <hr />
+        </form>`;  
+  },
+  commandParams : function(select, id) {
+    var paramDiv = $( select ).parent().next();
+    paramDiv.empty();
+    if(select.value == 'flywp')
+    {
+        paramDiv.append('<label for="waypoint">Select Waypoint</label>');
+        paramDiv.append('<select class="form-control" name="waypoint" id="params'+id+'"></select>');
+        App.contracts.Gunpla.deployed().then(function(instance) {
+            gunplaInstance = instance;
+        }).then(function() { return gunplaInstance.waypointCounter() })
+        .then(function(counter) {
+             console.log(counter);
+             for (var i = 0; i < counter; i++) {
+                gunplaInstance.wps_names(i).then(function(wp) {
+                    console.log(wp);
+                    console.log($( "#params"+id));
+                    $( "#params"+id).append('<option value="'+wp+'">'+wp+'</option>');
+                });
+            }
+        });
+    }
+
+
   }
 
 };
