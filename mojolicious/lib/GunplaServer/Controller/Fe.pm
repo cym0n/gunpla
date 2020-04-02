5603d48bc28c508cae47fb1087046d8b3c0d85b6
cymon <cymon@tiscali.it>
2020-04-02 23:54:51 +0200

    game to use as get parameter

--- mojolicious/lib/GunplaServer/Controller/Fe.pm
+++ mojolicious/lib/GunplaServer/Controller/Fe.pm
@@ -3,9 +3,9 @@ use Mojo::Base 'Mojolicious::Controller';
 
 # This action will render a template
 sub main {
-  my $self = shift;
-
-  $self->render();
+    my $c = shift;
+    my $game = $c->param('game');
+    $c->render(game => $game);
 }
 
 1;
