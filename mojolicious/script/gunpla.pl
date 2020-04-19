6d2810cf5493983dfd33f3f59ab2b8ee98eecae1
cymon <cymon@tiscali.it>
2020-04-19 20:29:13 +0200

    soft saving on daemon script

--- mojolicious/script/gunpla.pl
+++ mojolicious/script/gunpla.pl
@@ -44,7 +44,7 @@ elsif($command eq 'daemon')
 {
     while(1)
     {
-        my $world_obj = load_world($world);
+        my $world_obj = load_world($world, 1000);
         run_world($world_obj);
         sleep 10;    
     }
@@ -67,8 +67,9 @@ sub to_log
 sub load_world
 {
     my $name = shift;
+    my $save_every = shift;
     to_log("Loading world $name...");
-    my $world_obj = Gunpla::World->new(name => $name);
+    my $world_obj = Gunpla::World->new(name => $name, save_every => $save_every);
     $world_obj->load();
     if(! @{$world_obj->armies})
     {
@@ -80,14 +81,18 @@ sub load_world
 sub run_world
 {
     my $world_obj = shift;
+    my $steps = shift;
     if($world_obj->all_ready())
     {
         $world_obj->fetch_commands_from_mongo();
         if($world_obj->all_ready_and_fetched())
         {
-            my $e = $world_obj->action();
+            my $e = $world_obj->action($steps);
             $world_obj->save();
-            to_log("$e events generated. Mechas ready for new commands");
+            if($e)
+            {
+                to_log("$e events generated. Mechas ready for new commands");
+            }
         }
         else
         {
